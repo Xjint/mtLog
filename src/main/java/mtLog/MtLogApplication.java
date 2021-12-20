c7d1245b1226eec68d2655a345a40de6377e4974
xjint <877549050@qq.com>
2021-12-20 23:11:24 +0800

    init

---
+++ src/main/java/mtLog/MtLogApplication.java
@@ -0,0 +1,13 @@
+package mtLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtLogApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MtLogApplication.class, args);
+  }
+
+}
